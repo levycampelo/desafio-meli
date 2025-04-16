ae2bf55fea34d41bcfe34d5666b4447d00e2d5a6
Levy Campelo <levycampelo@gmail.com>
2025-04-16 19:04:52 +0000

    teste do flask

--- automacao/app.py
+++ automacao/app.py
@@ -1,1 +1,17 @@
-#
+from flask import Flask, render_template, request
+import os
+
+app = Flask(__name__, template_folder='interface')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        vlan = request.form.get('vlan')
+        hostname = request.form.get('hostname')
+        return f"VLAN: {vlan}, Hostname: {hostname}"
+    
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
